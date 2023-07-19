15df7ac8dedba5770942f4b96ed1dc99f369e01b
msaeltzer <msaeltze@mail.uni-mannheim.de>
2023-07-19 12:33:05 +0200

    adding structure of gatekeepR functions

---
+++ sessions/03_static_web_pages/gatekeepR.R
@@ -0,0 +1,55 @@
+
+
+## We first define a set of functions that can get different types of pages....
+
+
+
+### Crawling
+
+  ### search results
+
+
+
+
+
+### Scrapring
+
+    ### Author pages
+
+
+      #### Research Interest Score
+
+      #### Citations
+
+      #### H-Index
+
+      #### Name, Institution, Country
+
+
+   ### Author Network
+
+      #### Co-Authors
+
+      #### Cited this
+
+
+    ### Research Items
+
+      #### Article
+
+      #### Preprint
+
+      #### Book
+
+      #### Chapter
+
+      #### Coinference Paper
+
+      #### Patent
+
+      #### Data
+
+      #### Full-texts
+
+
+    ### Journals
